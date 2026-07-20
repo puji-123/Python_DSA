2168cc3aedb40422ef95c83030d6c96d0c47f665
pujitha <reddupujitha7702@gmail.com>
2026-07-20 12:30:47 +0530

    day1

---
+++ main.py
@@ -0,0 +1,48 @@
+"""print("pujitha\n reddy")
+print(r"\tcurrent\new\folder")
+def a(b):
+     
+    if b%2==0:
+        return "even"
+    else:
+        return "odd"
+print(a(3))
+d={}
+print(type(d))
+d={
+    "p":1
+    
+}
+d["p"]=12
+print(d)
+d={"p":1,
+   "u":2}
+print(d.get("p"))
+def count(*args):
+    print(type(args))
+    print(args)
+count(1,2,4,5)
+def a(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+a("p":1,"e":3)  
+a="pujitha"
+rev=""
+for i in range(len(a)):
+    rev=a[i]+rev
+b=list(rev)
+b.pop(0)
+b.pop()
+print("".join(b)) 
+while True:
+    print("p")
+n=int(input())
+sum=0
+for i in range(0,n+1):
+    sum=sum+i
+print(sum)"""
+
+    
+    
+
+
